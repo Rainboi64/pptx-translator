ceb251cc94a9358dc68175d2c881f950d0dc4242
Yaman Alhalabi <yamanalhalabi@Yamans-MacBook-Air.local>
2023-09-08 23:36:43 +0300

    first commit

---
+++ translate-pptx.py
@@ -0,0 +1,47 @@
+from pptx import Presentation
+import sys
+from googletrans import Translator
+
+prs = Presentation(sys.argv[2])
+translator = Translator(service_urls=['translate.googleapis.com'])
+
+dest = 'ar'
+i = 0
+
+for slide in prs.slides:
+    i = i + 1
+    shapes = []
+    
+    # only read in text-boxes
+    for shape in slide.shapes:
+        if(shape.has_text_frame):
+            if(shape.text != ''):
+                shapes.append(shape)
+    
+    # translate title
+    title = shapes[0].text
+    if sys.argv[1] == 'merge':
+            shapes[0].text = title + '\n\n' + translator.translate(title, dest=dest).text
+    elif sys.argv[1] == 'overwritte': 
+            shapes[0].text = translator.translate(title, dest=dest).text
+    else:
+            print('invalid command', sys.argv[1])
+
+    # translate content
+    for shape in shapes[1:]:
+            if(shape.text != ''): 
+                content = shape.text
+                translation = translator.translate(content, dest=dest).text
+
+            if sys.argv[1] == 'merge':
+                shape.text = content + '\n' + translation
+            elif sys.argv[1] == 'overwritte': 
+                shape.text = translation
+            else:
+                print('invalid command', sys.argv[1])
+
+
+    print('a slide was translated', i, '/', len(prs.slides))
+
+prs.save(sys.argv[3])
+print('done')
